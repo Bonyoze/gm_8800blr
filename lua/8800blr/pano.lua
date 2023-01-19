e97fd68408f61c297beb16fa00aebc39a6f96ff9
Bonyoze <59924045+Bonyoze@users.noreply.github.com>
2023-01-18 20:14:39 -0500

    minor changes

--- lua/8800blr/pano.lua
+++ lua/8800blr/pano.lua
@@ -25,7 +25,8 @@ local function init()
 	local viewing = false
 	local focusing = false
 	local focusTime
-	local oldKey, currKey
+	local oldKey
+	local currKey
 
 	local mat_icon = Material("8800blr/pano_icon.png", "smooth")
 	local mat_pano = CreateMaterial("8800blr_pano", "UnlitGeneric", { ["$vertexalpha"] = 1 })
@@ -40,64 +41,76 @@ local function init()
 	local color_pano = Color(255, 255, 255)
 	local matrix_pano = Matrix()
 
+	local LocalPlayer = LocalPlayer
+	local table_sort = table.sort
+	local string_format = string.format
+	local gui_IsGameUIVisible = gui.IsGameUIVisible
+	local vgui_GetKeyboardFocus = vgui.GetKeyboardFocus
+	local input_IsKeyDown = input.IsKeyDown
+	local render_SetMaterial = render.SetMaterial
+	local render_DrawQuadEasy = render.DrawQuadEasy
+	local cam_IgnoreZ = cam.IgnoreZ
+	local cam_PushModelMatrix = cam.PushModelMatrix
+	local cam_PopModelMatrix = cam.PopModelMatrix
+
 	local function draw()
 		local eyePos, eyeVector = EyePos(), EyeVector()
+		local time = SysTime()
 
 		-- sort data by pos distance
-		table.sort(PANO_DATA, function(a, b) return eyePos:DistToSqr(a.pos) < eyePos:DistToSqr(b.pos) end)
-		local nearest = PANO_DATA[1]
+		table_sort(PANO_DATA, function(a, b) return eyePos:DistToSqr(a.pos) < eyePos:DistToSqr(b.pos) end)
+		local closest = PANO_DATA[1]
 
 		-- update keys
 		oldKey = currKey
-		currKey = input.IsKeyDown(FOCUS_KEY)
+		currKey = input_IsKeyDown(FOCUS_KEY)
 
 		-- handle viewing/focusing
 		if not viewing then
-			if currKey and eyePos:DistToSqr(nearest.pos) <= FOCUS_DIST then
+			if not gui_IsGameUIVisible() and not vgui_GetKeyboardFocus()
+				and currKey and eyePos:DistToSqr(closest.pos) <= FOCUS_DIST then
 				if not focusing then
-					if not oldKey then
+					if not oldKey then -- start focusing
 						focusing = true
-						focusTime = SysTime()
+						focusTime = time
 					end
-				elseif SysTime() - focusTime >= FOCUS_LEN then
+				elseif time - focusTime >= FOCUS_LEN then -- start viewing/stop focusing
 					viewing = true
 					focusing = false
 				end
-			else
+			else -- cancel focusing
 				focusing = false
 			end
-		else
-			if not oldKey and currKey or LocalPlayer():GetAbsVelocity():Length() > 0 then
-				viewing = false
-			end
+		elseif not gui_IsGameUIVisible() and not vgui_GetKeyboardFocus()
+			and not oldKey and currKey or LocalPlayer():GetAbsVelocity():Length() > 0 then -- stop viewing
+			viewing = false
 		end
 
 		-- draw icons
 		if not viewing then
-			render.SetMaterial(mat_icon)
+			render_SetMaterial(mat_icon)
 			for i = PANO_TOTAL, 1, -1 do
-				render.DrawQuadEasy(PANO_DATA[i].pos, -eyeVector, 16, 16, color_white, 180)
+				render_DrawQuadEasy(PANO_DATA[i].pos, -eyeVector, 16, 16, color_white, 180)
 			end
 		end
 
 		-- draw pano
 		if viewing or focusing then
-			matrix_pano:SetTranslation(EyePos())
-			matrix_pano:SetAngles(nearest.ang)
+			matrix_pano:SetTranslation(eyePos)
+			matrix_pano:SetAngles(closest.ang)
 
-			color_pano.a = viewing and 255 or focusing and (SysTime() - focusTime) * 255 / FOCUS_LEN or 0
+			color_pano.a = viewing and 255 or (time - focusTime) * 255 / FOCUS_LEN
 
-			local index = nearest.index
-
-			cam.IgnoreZ(true)
-			cam.PushModelMatrix(matrix_pano)
+			cam_IgnoreZ(true)
+			cam_PushModelMatrix(matrix_pano)
+			local closestIndex = closest.index
 			for i = 1, 6 do
-				mat_pano:SetTexture("$basetexture", string.format("8800blr/pano/%03d_%d", index - 1, i - 1))
-				render.SetMaterial(mat_pano)
-				render.DrawQuadEasy(norms_pano[i] * -5, norms_pano[i], 10, 10, color_pano, i ~= 1 and i ~= 6 and 180 or 0)
+				mat_pano:SetTexture("$basetexture", string_format("8800blr/pano/%03d_%d", closestIndex - 1, i - 1))
+				render_SetMaterial(mat_pano)
+				render_DrawQuadEasy(norms_pano[i] * -5, norms_pano[i], 10, 10, color_pano, i ~= 1 and i ~= 6 and 180 or 0)
 			end
-			cam.PopModelMatrix()
-			cam.IgnoreZ(false)
+			cam_PopModelMatrix()
+			cam_IgnoreZ(false)
 		end
 	end
 
